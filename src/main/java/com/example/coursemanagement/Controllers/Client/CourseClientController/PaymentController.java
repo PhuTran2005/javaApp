68e934a9134b3e832e860222baa0831eed5b8717
Zị ó hỏ <phancongtri0301@gmail.com>
2025-09-16 22:19:04 +0700

    update code

--- src/main/java/com/example/coursemanagement/Controllers/Client/CourseClientController/PaymentController.java
+++ src/main/java/com/example/coursemanagement/Controllers/Client/CourseClientController/PaymentController.java
@@ -49,10 +49,6 @@ public class PaymentController implements Initializable {
 
 
     // Lấy studentId từ session
-    private final String accountNumber = "1518893947588";
-    private final String bankCode       = "MBBank";
-    private final int amount            = 10000;
-    private final String memo           = "nap";
     private final Alerts alerts = new Alerts();
     private final UserService userService = new UserService();
     private User myProfile;
